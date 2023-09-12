84bb0e1efda26eb51a2e9a5acbe3f7a0938532ef
Harriet Kim <harriet@Harriets-MacBook-Pro.local>
2023-09-12 01:03:08 -0400

    connected to CORS

--- word_cloud.py
+++ word_cloud.py
@@ -7,6 +7,8 @@ from flask import Flask, request, jsonify
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from flask_cors import CORS
+import os
 
 # Ensure nltk data is downloaded
 nltk.download('punkt')
@@ -14,6 +16,7 @@ nltk.download('stopwords')
 
 # Flask web server object
 app = Flask(__name__, static_folder='outputs')
+CORS(app)
 
 def get_tags(text, max_count):
     # Tokenize and filter out stopwords and non-alphabetic tokens
@@ -45,7 +48,7 @@ def process():
     content = request.json
     words = {}
     if content['words'] is not None:
-        for data in content['words'].values():
+        for data in content['words']:
             words[data['word']] = data['weight']
     process_from_text(content['text'], content['maxCount'], words, content['textID'])
     result = {'result': True}
@@ -56,5 +59,16 @@ def output():
     text_id = request.args.get('textID')
     return app.send_static_file(text_id + '.png')
 
+@app.route('/validate', methods=['GET', 'POST'])
+def validate():
+    text_id = request.args.get('textID')
+    path = "outputs/{0}.png".format(text_id)
+    result = {}
+    if os.path.isfile(path):
+        result['result'] = True
+    else:
+        result['result'] = False
+    return jsonify(result)
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000)
+    app.run('0.0.0.0', port=5000, threaded=True)
